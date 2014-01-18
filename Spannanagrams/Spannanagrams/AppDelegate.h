224457e1b285afca9ce71d3e7ba7a525fc0fe9f5
Stephen Rodriguez <steprodriguez10@gmail.com>
2014-01-18 00:57:58 -0500

    initial commit w/ xcode

---
+++ Spannanagrams/Spannanagrams/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Spannanagrams
+//
+//  Created by Stephen Rodriguez on 1/18/14.
+//  Copyright (c) 2014 Stephen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Spannanagrams/Spannanagrams/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Spannanagrams
+//
+//  Created by Stephen Rodriguez on 1/18/14.
+//  Copyright (c) 2014 Stephen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
